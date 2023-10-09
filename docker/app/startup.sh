7b8f6c9204d357fdecb066c07b257afc1c603584
vijay <vijaysangoi29@gmail.com>
2023-10-09 20:28:14 +0530

    docker phase 5.0

---
+++ docker/app/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+    touch /$CONTAINER_FIRST_STARTUP
+    sleep 60
+    cd var/www/nothing_todo
+    php artisan optimize
+    php artisan migrate:fresh
+fi
+apachectl -D FOREGROUND
