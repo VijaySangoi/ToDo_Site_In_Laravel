0a08315cee9b3cdd68c0ea29650842c23872e8c9
vijay <vijaysangoi29@gmail.com>
2023-10-10 14:18:31 +0530

    docker phase 6.0

--- docker/app/startup.sh
+++ docker/app/startup.sh
@@ -3,7 +3,7 @@
 CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
 if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
     touch /$CONTAINER_FIRST_STARTUP
-    sleep 60
+    sleep 120
     cd var/www/nothing_todo
     php artisan optimize
     php artisan migrate:fresh
